d2f1e3fcdbcb0eac51f33b3d888e5fc8830bdb19
comicwang <672802899@qq.com>
2018-08-09 00:16:01 +0800

    修改图片查看器

--- MMView/MMView/FormMain.Designer.cs
+++ MMView/MMView/FormMain.Designer.cs
@@ -28,6 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.title = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.netUrl = new System.Windows.Forms.DataGridViewLinkColumn();
@@ -38,7 +40,19 @@
             this.dirPath = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.scan = new System.Windows.Forms.DataGridViewImageColumn();
             this.View = new System.Windows.Forms.DataGridViewLinkColumn();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menu_refreash = new System.Windows.Forms.ToolStripMenuItem();
+            this.winFormPager1 = new Kenny.Controls.WinForm.WinFormPager();
+            this.pnlView = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.menu_view = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.pnlView.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
@@ -56,11 +70,12 @@
             this.scan,
             this.View});
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Location = new System.Drawing.Point(0, 25);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 23;
-            this.dataGridView1.Size = new System.Drawing.Size(744, 422);
+            this.dataGridView1.Size = new System.Drawing.Size(700, 481);
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // title
             // 
@@ -110,16 +125,113 @@
             this.View.HeaderText = "查看";
             this.View.Name = "View";
             // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.menu_refreash,
+            this.menu_view});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(700, 25);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(68, 21);
+            this.toolStripMenuItem1.Text = "启动爬虫";
+            // 
+            // menu_refreash
+            // 
+            this.menu_refreash.Name = "menu_refreash";
+            this.menu_refreash.Size = new System.Drawing.Size(68, 21);
+            this.menu_refreash.Text = "刷新数据";
+            this.menu_refreash.Click += new System.EventHandler(this.menu_refreash_Click);
+            // 
+            // winFormPager1
+            // 
+            this.winFormPager1.AutoSize = true;
+            this.winFormPager1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.winFormPager1.BackColor = System.Drawing.Color.Transparent;
+            this.winFormPager1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("winFormPager1.BackgroundImage")));
+            this.winFormPager1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.winFormPager1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.winFormPager1.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.winFormPager1.Location = new System.Drawing.Point(0, 506);
+            this.winFormPager1.Name = "winFormPager1";
+            this.winFormPager1.PageSize = 100;
+            this.winFormPager1.RecordCount = 0;
+            this.winFormPager1.Size = new System.Drawing.Size(700, 27);
+            this.winFormPager1.TabIndex = 2;
+            this.winFormPager1.PageIndexChanged += new Kenny.Controls.WinForm.WinFormPager.EventHandler(this.winFormPager1_PageIndexChanged);
+            // 
+            // pnlView
+            // 
+            this.pnlView.Controls.Add(this.pictureBox1);
+            this.pnlView.Controls.Add(this.listView1);
+            this.pnlView.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pnlView.Location = new System.Drawing.Point(700, 0);
+            this.pnlView.Name = "pnlView";
+            this.pnlView.Size = new System.Drawing.Size(350, 533);
+            this.pnlView.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 183);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(350, 350);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // listView1
+            // 
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(350, 183);
+            this.listView1.SmallImageList = this.imageList1;
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.SmallIcon;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // menu_view
+            // 
+            this.menu_view.Name = "menu_view";
+            this.menu_view.Size = new System.Drawing.Size(68, 21);
+            this.menu_view.Text = "关闭查看";
+            this.menu_view.Click += new System.EventHandler(this.menu_view_Click);
+            // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(744, 422);
+            this.ClientSize = new System.Drawing.Size(1050, 533);
             this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.winFormPager1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.pnlView);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "FormMain";
             this.Text = "MM查看";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.pnlView.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -135,6 +247,15 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn dirPath;
         private System.Windows.Forms.DataGridViewImageColumn scan;
         private System.Windows.Forms.DataGridViewLinkColumn View;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem menu_refreash;
+        private Kenny.Controls.WinForm.WinFormPager winFormPager1;
+        private System.Windows.Forms.Panel pnlView;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ToolStripMenuItem menu_view;
     }
 }
 
--- MMView/MMView/FormMain.cs
+++ MMView/MMView/FormMain.cs
@@ -19,15 +19,28 @@ namespace MMView
         {
             InitializeComponent();
 
+            winFormPager1.RecordCount = Query(winFormPager1.PageIndex, winFormPager1.PageSize);
+        }
+
+        private string dir = string.Empty;
+
+        private int Query(int pageIndex, int pageSize)
+        {
             var client = new MongoClient("mongodb://localhost:27017");
 
             var database = client.GetDatabase("mm_database");
 
             var collection = database.GetCollection<BsonDocument>("mm_collect");
 
-            var document = collection.Find(new BsonDocument()).ToList();
+            var query = collection.AsQueryable<BsonDocument>();
 
-            for (int i = 0; i < 30; i++)
+            int total = query.Count();
+
+            var document = query.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToList();
+
+            dataGridView1.Rows.Clear();
+
+            for (int i = 0; i < document.Count; i++)
             {
                 int index = dataGridView1.Rows.Add();
                 dataGridView1[0, index].Value = document[i][1];
@@ -39,8 +52,73 @@ namespace MMView
                 dataGridView1[6, index].Value = document[i][7];
                 //获取第一张图片
                 string[] lstUrl = Directory.GetFiles(document[i][7].AsString);
-                ((DataGridViewImageCell)dataGridView1[7, index]).Value = Image.FromFile(lstUrl[0]);
+                if (lstUrl.Length > 0)
+                    ((DataGridViewImageCell)dataGridView1[7, index]).Value = Image.FromFile(lstUrl[0]);
                 dataGridView1[8, index].Value = "查看源";
+            }
+
+            return total;
+        }
+
+        private void winFormPager1_PageIndexChanged(object sender, EventArgs e)
+        {
+            winFormPager1.RecordCount = Query(winFormPager1.PageIndex, winFormPager1.PageSize);
+        }
+
+        private void menu_refreash_Click(object sender, EventArgs e)
+        {
+            winFormPager1.RecordCount = Query(winFormPager1.PageIndex, winFormPager1.PageSize);
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && e.ColumnIndex == View.DisplayIndex)
+            {
+                dir = dataGridView1[6, e.RowIndex].Value.ToString();
+
+                imageList1.Images.Clear();
+
+                string[] files = Directory.GetFiles(dir);
+
+                if (files.Length > 0)
+                {
+                    foreach (var item in files)
+                    {
+                        string fileName = Path.GetFileNameWithoutExtension(item);
+                        imageList1.Images.Add(fileName, Image.FromFile(item));
+                        listView1.Items.Add(fileName, fileName);
+                    }
+                    if (pnlView.Visible == false)
+                    {
+                        menu_view_Click(null, null);
+                    }
+                    pictureBox1.Image = Image.FromFile(files[0]);
+
+                }
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                ListViewItem lv = listView1.SelectedItems[0];
+
+                pictureBox1.Image = Image.FromFile(Path.Combine(dir, lv.Text + ".jpg"));
+            }
+        }
+
+        private void menu_view_Click(object sender, EventArgs e)
+        {
+            if(menu_view.Text=="关闭查看")
+            {
+                pnlView.Visible = false;
+                menu_view.Text = "打开查看";
+            }
+            else
+            {
+                pnlView.Visible = true;
+                menu_view.Text = "关闭查看";
             }
         }
     }
